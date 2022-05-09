4e33a32e0486851839195c2a9c22e35c0ea27cf4
meytal benchimol <meytal09@gmail.com>
2022-05-09 10:01:23 +0300

    start const

--- include/Tile.h
+++ include/Tile.h
@@ -15,21 +15,21 @@ class Tile
 public:
 	Tile(const sf::Vector2f position,float scale, bool = false,int=0 ,int=0);
 
-    void setPosition(const sf::Vector2f position);
+    void setPosition(const sf::Vector2f position); // Integrity check
     void updateAndDraw(sf::RenderWindow& window);
     bool clicked(sf::Vector2f mousePos);
-    auto getBegin() { return m_neighbors.begin(); };
-    auto getEnd() { return m_neighbors.end(); };
+    const auto getBegin() { return m_neighbors.begin(); };
+    const auto getEnd() { return m_neighbors.end(); };
     void addNeighbor(Tile *);
-    void setColor(colorId);
-    bool isLimit() { return m_isLimit; }; //todo const
-    void setFoundBy(Tile* tile) { m_foundBy = tile; };
+    void setColor(colorId); // Integrity check
+    const bool isLimit() { return m_isLimit; }; 
+    void setFoundBy(Tile* tile) { m_foundBy = tile; }; // Integrity check
     void checkNeighbors();
 
     sf::Sprite getSprite() const;
-    sf::Vector2<float> getLocation( ) const;
+    sf::Vector2<float> getLocation() const;
     Tile* getFoundBy() { return m_foundBy; };
-    colorId getColor() { return m_color; }; // todo const
+    const colorId getColor() { return m_color; }; 
 
 private:
     bool m_isClicked = false;
@@ -39,5 +39,5 @@ private:
     Tile* m_foundBy = nullptr;   
     sf::Texture *m_texture;
     sf::Sprite m_sprite;
-    std::list<Tile*> m_neighbors; // sherdptr
+    std::list<Tile*> m_neighbors; 
 };
